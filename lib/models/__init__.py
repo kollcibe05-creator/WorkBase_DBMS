2c563b55e6c435af61158e65e7115283babc3363
Collins Kibet <kollcibe05@gmail.com>
2025-12-05 09:04:12 +0300

    recreated init file

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,4 +1,21 @@
-import sqlite3
 
-CONN = sqlite3.connect('company.db')
-CURSOR = CONN.cursor()
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+database = 'sqlite:///workbase.db' 
+
+
+engine = create_engine(database)
+
+
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+
+def recreate_db():
+    """Drops all tables and creates them based on the new Base metadata."""
+
+    Base.metadata.drop_all(engine)        
+    Base.metadata.create_all(engine)
